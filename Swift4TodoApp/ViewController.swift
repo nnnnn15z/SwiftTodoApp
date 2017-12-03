0a780aa6d803fdaaf6df541509e0fa7dd02653bf
ryo.takahashi <b152029@kca.ac.jp>
2017-12-03 20:31:01 +0900

    Initial Commit

---
+++ Swift4TodoApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift4TodoApp
+//
+//  Created by AdminAir on 2017/12/03.
+//  Copyright © 2017年 ryotakahashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
